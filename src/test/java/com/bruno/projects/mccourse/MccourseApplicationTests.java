794ca48e023ebcebdf74768a9b57cae804a1c876
Bruno Soares <brunosoaresbsv@gmail.com>
2020-06-19 19:38:48 -0300

    Project created

---
+++ src/test/java/com/bruno/projects/mccourse/MccourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.projects.mccourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
