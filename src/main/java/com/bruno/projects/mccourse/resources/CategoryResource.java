9dbc24422442b13ed6fb6e8b84072c342c8d9bdd
Bruno Soares <brunosoaresbsv@gmail.com>
2020-06-19 20:07:34 -0300

    Testing first domain class: Category

--- src/main/java/com/bruno/projects/mccourse/resources/CategoryResource.java
+++ src/main/java/com/bruno/projects/mccourse/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.bruno.projects.mccourse.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bruno.projects.mccourse.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 	
 	@GetMapping
-	public String list() {
-		return "REST works!";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }
