abeaf8e3e6cb67f11260248d47d803a0f568f424
Bruno Soares <brunosoaresbsv@gmail.com>
2020-06-19 21:05:09 -0300

    Creating instantiation operation

--- src/main/java/com/bruno/projects/mccourse/MccourseApplication.java
+++ src/main/java/com/bruno/projects/mccourse/MccourseApplication.java
@@ -1,13 +1,32 @@
 package com.bruno.projects.mccourse;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bruno.projects.mccourse.domain.Category;
+import com.bruno.projects.mccourse.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class MccourseApplication {
+public class MccourseApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MccourseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
